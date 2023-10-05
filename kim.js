9fc28aaed78b21fe0fd64a6364151eb7b543dcd4
peeChulChul <eyeles356@naver.com>
2023-10-05 09:18:08 +0900

    개인페이지 자바스크립트 수정(버튼클릭)

--- kim.js
+++ kim.js
@@ -1,17 +1,17 @@
 const dropDownBox = document.querySelector('.dropDownBox');
 const main = document.querySelector('main');
 const children = main.children;
+const answerBox = document.querySelector('.answer');
+const btns = document.querySelectorAll('.btn-q');
+
 let counte = 0;
-dropDownBox.addEventListener('click', () => {
-    dropDownBox.classList.toggle('active');
-});
 
 function removeClickMain() {
     main.removeEventListener('click', onClickMain);
 }
 
 function onClickMain() {
-    if (children.length === counte + 1) {
+    if (children.length === counte + 2) {
         removeClickMain();
     }
     children[counte].classList.remove('hide');
@@ -19,10 +19,18 @@ function onClickMain() {
     counte++;
 }
 main.addEventListener('click', onClickMain);
-// main.addEventListener('click', () => {
-//     if (children.length === counte) {
-//     }
-//     children[counte].classList.remove('hide');
-//     console.log(children[counte]);
-//     counte++;
-// });
+
+dropDownBox.addEventListener('click', () => {
+    dropDownBox.classList.toggle('active');
+});
+
+function onClickBtn(e) {
+    const value = e.target.value;
+    Array.from(answerBox.children).forEach((children) => {
+        children.classList.add('hide');
+        if (Array.from(children.classList).includes(value)) {
+            children.classList.remove('hide');
+        }
+    });
+}
+btns.forEach((btn) => btn.addEventListener('click', onClickBtn));
